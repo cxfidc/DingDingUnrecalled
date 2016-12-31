5048830060a159bc1e6cd7caa240ab4eee758e03
veryyoung <codingyoung@gmail.com>
2016-12-31 16:04:23 +0800

    防止不开插件的情况下，撤回的消息变成'Msg has been recalled.'。 #1

--- app/src/main/java/me/veryyoung/dingding/unrecalled/Main.java
+++ app/src/main/java/me/veryyoung/dingding/unrecalled/Main.java
@@ -1,4 +1,4 @@
-package me.veryyoung.dingding.unrecallled;
+package me.veryyoung.dingding.unrecalled;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -9,29 +9,32 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
+import static de.robv.android.xposed.XposedHelpers.newInstance;
 
 
 public class Main implements IXposedHookLoadPackage {
 
     private static final String DINGDING_PACKAGE_NAME = "com.alibaba.android.rimet";
 
-    private static final String MAP_CLASS_NAME = "com.alibaba.wukong.im.message.MessageImpl";
-    private static final String MAP_FUNCTION_NAME = "recallStatus";
+    private static final String MESSAGE_CLASS_NAME = "com.alibaba.wukong.im.message.MessageImpl";
     private static final String CONVERSATION_CLASS_NAME = "com.alibaba.wukong.im.conversation.ConversationImpl";
+    private static final String MESSAGE_DS_CLASS_NAME = "cuw";
     private static final String RECALLED_MSG_TEXT = "Msg has been recalled.";
     private static final int NUM_MESSAGE_TEXT_TYPE = 1;
 
 
-    @SuppressWarnings("unchecked")
     @Override
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 
 
         if (lpparam.packageName.equals(DINGDING_PACKAGE_NAME)) {
 
-            //map
-            findAndHookMethod(MAP_CLASS_NAME, lpparam.classLoader, MAP_FUNCTION_NAME, new XC_MethodHook() {
+            findAndHookMethod(MESSAGE_CLASS_NAME, lpparam.classLoader, "recallStatus", new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                     // if msg is already recalled and RECALLED msg is stored in local DB,
@@ -45,13 +48,13 @@ public class Main implements IXposedHookLoadPackage {
             });
 
             // stop replacing message content with default recalled string in database
-            XposedHelpers.findAndHookMethod(MAP_CLASS_NAME, lpparam.classLoader, "b", String.class, Collection.class, new XC_MethodHook() {
+            findAndHookMethod(MESSAGE_DS_CLASS_NAME, lpparam.classLoader, "b", String.class, Collection.class, new XC_MethodHook() {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    final Class<?> CLASS_MESSAGE_DB = XposedHelpers.findClass(MAP_CLASS_NAME, lpparam.classLoader);
-                    final String dbName = (String) XposedHelpers.callStaticMethod(CLASS_MESSAGE_DB, "getReadableDatabase");
+                    final Class<?> messageDS = findClass(MESSAGE_DS_CLASS_NAME, lpparam.classLoader);
+                    final String dbName = (String) callStaticMethod(messageDS, "getReadableDatabase");
 
-                    Object conversation = XposedHelpers.newInstance(XposedHelpers.findClass(CONVERSATION_CLASS_NAME, lpparam.classLoader));
+                    Object conversation = newInstance(findClass(CONVERSATION_CLASS_NAME, lpparam.classLoader));
 
                     String cid = (String) param.args[0];
 
@@ -63,12 +66,12 @@ public class Main implements IXposedHookLoadPackage {
                         } else {
                             long mid = XposedHelpers.getLongField(o, "mMid");
                             // load messageImpl from DB
-                            Object msgImpl = XposedHelpers.callStaticMethod(CLASS_MESSAGE_DB, "a", cid, mid, conversation);
+                            Object msgImpl = callStaticMethod(messageDS, "a", cid, mid, conversation);
                             String originalMsgText = getMsgText(msgImpl);
                             // append info suffix
                             setMsgText(msgImpl, originalMsgText + " [已撤回]");
                             // write messageImpl back into DB
-                            XposedHelpers.callStaticMethod(CLASS_MESSAGE_DB, "a", dbName, cid, Collections.singletonList(msgImpl));
+                            callStaticMethod(messageDS, "a", dbName, cid, Collections.singletonList(msgImpl));
                         }
                     }
 
@@ -78,47 +81,24 @@ public class Main implements IXposedHookLoadPackage {
         }
     }
 
-    /**
-     * Load text from messageImpl instance.
-     *
-     * @param msg message
-     * @return message string, null if not text type
-     */
+
     private String getMsgText(Object msg) {
-        try {
-            // msg.class = com.alibaba.wukong.im.MessageImpl
-            Object innerContent = XposedHelpers.getObjectField(msg, "mMessageContent");
-            // indicate which type of content
-            int type = (int) XposedHelpers.callMethod(innerContent, "type");
+        Object innerContent = getObjectField(msg, "mMessageContent");
+        int type = (int) callMethod(innerContent, "type");
 
-            if (type == NUM_MESSAGE_TEXT_TYPE) {
-                // innerContent.class = com.alibaba.wukong.im.message.MessageContentImpl$TextContentImpl
-                return (String) XposedHelpers.callMethod(innerContent, "text");
-            }
-        } catch (Throwable t) {
+        if (type == NUM_MESSAGE_TEXT_TYPE) {
+            return (String) callMethod(innerContent, "text");
         }
-
         return null;
     }
 
-    /**
-     * Set text into messageImpl instance.
-     *
-     * @param msg     message
-     * @param newText text to be set
-     */
     private void setMsgText(Object msg, String newText) {
-        try {
-            // msg.class = com.alibaba.wukong.im.MessageImpl
-            Object innerContent = XposedHelpers.getObjectField(msg, "mMessageContent");
-            // indicate which type of content
-            int type = (int) XposedHelpers.callMethod(innerContent, "type");
+        Object innerContent = getObjectField(msg, "mMessageContent");
+        int type = (int) callMethod(innerContent, "type");
 
-            if (type == NUM_MESSAGE_TEXT_TYPE) {
-                // innerContent.class = com.alibaba.wukong.im.message.MessageContentImpl$TextContentImpl
-                XposedHelpers.callMethod(innerContent, "setText", newText);
-            }
-        } catch (Throwable t) {
+        if (type == NUM_MESSAGE_TEXT_TYPE) {
+            callMethod(innerContent, "setText", newText);
         }
     }
+
 }
